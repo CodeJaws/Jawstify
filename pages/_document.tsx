2ab494483360579533d34c66423dddfed5d23713
Soeun Kim <summerkim0825@gmail.com>
2023-12-18 17:50:12 +0900

    Refactor: _document 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,40 @@
-import type { DocumentContext, DocumentInitialProps } from 'next/document'
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+    ctx.renderPage = () =>
+      originalRenderPage({
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      }
-    } finally {
-      sheet.seal()
-    }
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
+
+export default MyDocument
