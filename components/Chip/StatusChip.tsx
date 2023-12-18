59164abbccf70e8da87d4a1f8afb72fd89d67087
oauch <lgm9515@naver.com>
2023-12-18 18:05:13 +0900

    [#8] Feat: StatusChip 컴포넌트

---
+++ components/Chip/StatusChip.tsx
@@ -0,0 +1,33 @@
+import Ellipse from '@/public/assets/icons/ChipEllipse.svg';
+import { selectSize } from '@/utils/selectFontSize';
+import Image from 'next/image';
+import { styled } from 'styled-components';
+
+interface StatusChipProps {
+  size: string;
+  content: string;
+}
+
+function StatusChip({ size, content }: StatusChipProps) {
+  const fontSize = selectSize(size);
+  return (
+    <StyledContainer fontSize={fontSize}>
+      <Image width={6} height={6} src={Ellipse} alt="동그라미" />
+      {content}
+    </StyledContainer>
+  );
+}
+
+export default StatusChip;
+
+const StyledContainer = styled.div<{ fontSize: number }>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  padding: 4px 8px;
+  border-radius: 11px;
+  font-size: ${(props) => props.fontSize}px;
+  color: #5534da;
+  background-color: #f1effd;
+`;
