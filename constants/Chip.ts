30c6cf91792e6535e47caa3317b55a5e13b662a9
oauch <lgm9515@naver.com>
2023-12-18 18:04:13 +0900

    [#8] Add: StatusChip 텍스트 상수화

--- constants/Chip.ts
+++ constants/Chip.ts
@@ -1,6 +1,10 @@
+const TO_DO = 'To Do';
+const ON_PROGRESS = 'On Progress';
+const DONE = 'Done';
+
 const PROJECT = '프로젝트';
 const NORMAL = '일반';
 const BACK_END = '백엔드';
 const MAX_LEVEL = '상';
 
-export { BACK_END, MAX_LEVEL, NORMAL, PROJECT };
+export { BACK_END, DONE, MAX_LEVEL, NORMAL, ON_PROGRESS, PROJECT, TO_DO };
