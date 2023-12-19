cb84db9704f5c1518b50c4343f63be6e3537e598
oauch <lgm9515@naver.com>
2023-12-19 17:44:18 +0900

    [#25] Feat: DropDownMenu 컴포넌트

---
+++ components/ModalDropDown/DropDownMenu.tsx
@@ -0,0 +1,78 @@
+import StatusChip from '@/components/Chip/StatusChip';
+import Check from '@/public/assets/icons/GrayCheck.svg';
+import { COLORS } from '@/styles/palettes';
+
+import Image from 'next/image';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { styled } from 'styled-components';
+
+interface DropDownMenuProps {
+  isOpen: boolean;
+  setStatus: Dispatch<SetStateAction<string>>;
+}
+
+const Status = [
+  { id: 0, content: 'To Do' },
+  { id: 1, content: 'On Progress' },
+  { id: 2, content: 'Done' },
+];
+
+function DropDownMenu({ isOpen, setStatus }: DropDownMenuProps) {
+  const [isCheck, setIsCheck] = useState(0);
+  const handleCheck = (id: number, content: string) => {
+    setIsCheck(id);
+    setStatus(content);
+  };
+
+  return (
+    <StyledContainer isOpen={isOpen}>
+      <StyledWrapper>
+        <StyledInWrapper>
+          {Status.map((val) => (
+            <StyledButton key={val.id} onClick={() => handleCheck(val.id, val.content)}>
+              {isCheck === val.id ? <Image src={Check} alt="체크 이미지" /> : <StyledBlank />}
+              <StatusChip content={val.content} />
+            </StyledButton>
+          ))}
+        </StyledInWrapper>
+      </StyledWrapper>
+    </StyledContainer>
+  );
+}
+
+export default DropDownMenu;
+
+const StyledContainer = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  position: absolute;
+  top: 50px;
+  left: 0;
+  width: 217px;
+  height: 118px;
+  border: 1px solid ${COLORS.GRAY_30};
+  border-radius: 6px;
+  background: ${COLORS.WHITE};
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
+  padding: 13px 8px;
+`;
+
+const StyledWrapper = styled.button`
+  display: flex;
+  gap: 6px;
+`;
+
+const StyledInWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  gap: 6px;
+`;
+
+const StyledBlank = styled.div`
+  width: 22px;
+  height: 22px;
+`;
