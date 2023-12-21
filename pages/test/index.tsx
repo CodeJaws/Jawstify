f8a55578768c5ad54e0e82119cb0bb8e749ff028
zermzerm <zerm0907@gmail.com>
2023-12-21 15:00:20 +0900

    [#39] Fix: test파일 초기화

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,38 +1,6 @@
-import DashBoardButton from '@/components/common/Button/DashBoardButton';
-import ColumnAddButton from '@/components/common/Button/ColumnAddButton';
-import DeleteButton from '@/components/common/Button/DeleteButton';
-import LoginButton from '@/components/common/Button/LoginButton';
-import DashBoardAddButton from '@/components/common/Button/DashBoardAddButton';
-import AddButton from '@/components/common/Button/AddButton';
-import PaginationButton from '@/components/common/Button/PaginationButton';
-import styled from 'styled-components';
-import Button from '@/components/common/Button/Button';
-import TwinButton from '@/components/common/Button/TwinButton';
-
-function TestPage() {
-  return (
-    <>
-      <ButtonDiv>
-        <LoginButton type="login" active={true} />
-        <ColumnAddButton />
-        <AddButton />
-        <DeleteButton />
-        <PaginationButton active={true} />
-        <DashBoardAddButton />
-        <DashBoardButton text="대시보드" color="green" king={true} />
-        {/* size = large or small / isViolet은 보라색 배경 바꾸기 */}
-        <Button size="small" isViolet={true} text="성공" />
-        <TwinButton size="small" text1="수락" text2="거절" isViolet={false} />
-      </ButtonDiv>
-    </>
-  );
+/** Test용 Page입니다. */
+function Test() {
+  return <></>;
 }
 
-export default TestPage;
-
-const ButtonDiv = styled.div`
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
+export default Test;
