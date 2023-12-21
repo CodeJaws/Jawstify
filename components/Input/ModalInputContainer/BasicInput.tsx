a514b022da8e7d3d2b250402b0986985a512fbcb
Yeon Kim <46954114+arky02@users.noreply.github.com>
2023-12-21 16:28:23 +0900

    Merge pull request #37 from part3-4/feat/Input_#12
    [#12] Input 컴포넌트

---
+++ components/Input/ModalInputContainer/BasicInput.tsx
@@ -0,0 +1,91 @@
+import { fontStyle } from '@/styles/fontStyle';
+import { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
+import { COLORS } from '@/styles/palettes';
+import { StyledErrorText, StyledInput, StyledInputContainer, StyledLabel, VioletStar } from '../Input.style';
+import { NO_VALUE_ERROR } from '@/constants/Input';
+
+interface Props {
+  label: string;
+  inputValue?: string;
+  placeholder?: string;
+  errorMessage?: string;
+  isNecessary?: boolean;
+  isTextArea?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+}
+
+/**
+ * Modal Basic Input
+ * @param label input 라벨 텍스트
+ * @param inputValue 부모 컴포넌트에서 제어하는 input 텍스트 값
+ * @param placeholder input placeholder 텍스트
+ * @param errorMessage 부모 컴포넌트에서 제어하는 input에 띄우고자 하는 에러 메세지, exist하면 error design 표시
+ * @param isNecessary 필수 입력 칸 여부, true일 경우 별표 표시
+ * @param isTextArea textArea 여부, true일 경우 input 대신 textarea return
+ * @param onChange 부모 컴포넌트에서 제어하는 input onChange 함수
+ * */
+function BasicInput({
+  label = '',
+  inputValue = '',
+  placeholder = '',
+  errorMessage = '',
+  isNecessary = false,
+  isTextArea = false,
+  onChange,
+}: Props) {
+  const [isNoValue, setIsNoValue] = useState<boolean>(false);
+
+  const defaultPlaceholder = label + '을 입력해 주세요';
+  const hasError = errorMessage !== '';
+
+  const handleBlur = () => {
+    isNecessary && inputValue === '' ? setIsNoValue(true) : setIsNoValue(false);
+  };
+  return (
+    <StyledInputContainer>
+      <StyledLabel>
+        {label}
+        {isNecessary && <VioletStar>*</VioletStar>}
+      </StyledLabel>
+      {isTextArea ? (
+        <StyledTextarea
+          value={inputValue}
+          placeholder={placeholder || defaultPlaceholder}
+          $error={isNoValue || hasError}
+          onChange={onChange}
+          onBlur={handleBlur}
+        />
+      ) : (
+        <StyledInput
+          value={inputValue}
+          placeholder={placeholder || defaultPlaceholder}
+          $error={isNoValue || hasError}
+          onChange={onChange}
+          onBlur={handleBlur}
+        />
+      )}
+      {(isNoValue || hasError) && <StyledErrorText>{errorMessage || NO_VALUE_ERROR}</StyledErrorText>}
+    </StyledInputContainer>
+  );
+}
+
+export default BasicInput;
+
+const StyledTextarea = styled.textarea<{ $error: boolean }>`
+  width: 100%;
+  padding: 15px 16px;
+  border-radius: 8px;
+  border: 1px solid ${({ $error }) => ($error ? COLORS.RED_D6 : COLORS.GRAY_D9)};
+  background-color: ${COLORS.WHITE_FF};
+  color: ${COLORS.BLACK_33};
+  ${fontStyle(16, 400)}
+
+  &:hover,
+  &:focus,
+  &:active {
+    border: 1px solid ${({ $error }) => ($error ? COLORS.RED_D6 : COLORS.VIOLET_55)};
+    color: ${COLORS.BLACK_33};
+    outline: none;
+  }
+`;
---
+++ constants/Input.ts
@@ -0,0 +1,37 @@
+import { COLORS } from '@/styles/palettes';
+
+export const NO_VALUE_ERROR = '값을 입력해 주세요.';
+
+export const EMAIL_ERROR = {
+  FORMAT_ERROR: '이메일 형식으로 작성해 주세요.',
+};
+
+export const PWD_ERROR = {
+  FORMAT_ERROR: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.',
+};
+
+export const PWD_CHECK_ERROR = {
+  PWD_NOT_SAME: '비밀번호가 일치하지 않아요.',
+};
+
+export const DEFAULT_PLACEHOLDER = {
+  EMAIL: '이메일을 입력해 주세요',
+  PWD: '8자 이상 입력해 주세요',
+  PWD_CHECK: '비밀번호를 한번 더 입력해 주세요',
+  COMMENT: '댓글 작성하기',
+  DATE: '날짜를 입력해 주세요',
+  TAG: '입력 후 Enter',
+  NICKNAME: '닉네임을 입력해 주세요',
+};
+
+// <-- Modal Input -->
+export const TAG_COLOR = [
+  [COLORS.ORANGE_F9, COLORS.ORANGE_D5],
+  [COLORS.GREEN_E7, COLORS.GREEN_86],
+  [COLORS.PINK_F7, COLORS.PINK_D5],
+  [COLORS.BLUE_DB, COLORS.BLUE_49],
+];
+export const TAG_DATA = [].map((item) => ({
+  value: item,
+  label: item,
+}));
