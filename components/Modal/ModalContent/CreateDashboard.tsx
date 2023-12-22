c8c1fd2f64b31d746a550342dbed8089929a0a74
arky02 <kyean07@naver.com>
2023-12-23 06:08:41 +0900

    [#41] feat: CreateDashboard Modal 구현 완료

--- components/Modal/ModalContent/CreateDashboard.tsx
+++ components/Modal/ModalContent/CreateDashboard.tsx
@@ -1,20 +1,59 @@
-import { fontStyle } from '@/styles/fontStyle';
+import ColorChip from '@/components/Chip/ColorChip';
+import BasicInput from '@/components/Input/ModalInputContainer/BasicInput';
+import TwinButton from '@/components/common/Button/TwinButton';
+import { onMobile } from '@/styles/mediaQuery';
 import styled from 'styled-components';
 
-interface Props {}
+interface Props {
+  onOkClick: () => void;
+  onCancelClick: () => void;
+}
 
-function CreateDashboard() {
-  return <StyledContainer></StyledContainer>;
+function CreateDashboard({ onCancelClick, onOkClick }: Props) {
+  return (
+    <>
+      <StyledContainer>
+        <BasicInput label="대시보드 이름" placeholder="뉴 프로젝트"></BasicInput>
+        <ColorChip />
+      </StyledContainer>
+
+      <StyledButtonContainer>
+        <StyledTwinButton
+          text1="취소"
+          text2="생성"
+          size="large"
+          onLeftClick={onCancelClick}
+          onRightClick={onOkClick}
+        ></StyledTwinButton>
+      </StyledButtonContainer>
+    </>
+  );
 }
 
 export default CreateDashboard;
 
 const StyledContainer = styled.div`
-  /* padding: 100px 160px; */
   white-space: nowrap;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 18px;
 `;
 
-const StyledDescription = styled.h5`
-  ${fontStyle(18, 500)}
-  text-align: center
+const StyledButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 2.4rem;
+
+  ${onMobile} {
+    justify-content: center;
+  }
+`;
+
+const StyledTwinButton = styled(TwinButton)`
+  & > button {
+    border-radius: 8px;
+  }
 `;
