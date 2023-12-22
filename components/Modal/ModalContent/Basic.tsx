363877b22785de7207608f32422082b9aa0c6ddd
arky02 <kyean07@naver.com>
2023-12-23 06:07:29 +0900

    [#41] feat: Basic Modal 구현

--- components/Modal/ModalContent/Basic.tsx
+++ components/Modal/ModalContent/Basic.tsx
@@ -1,26 +1,55 @@
-import { fontStyle } from '@/styles/fontStyle';
+import BasicInput from '@/components/Input/ModalInputContainer/BasicInput';
+import TwinButton from '@/components/common/Button/TwinButton';
+import { onMobile } from '@/styles/mediaQuery';
 import styled from 'styled-components';
 
 interface Props {
-  description: string;
+  type: '초대하기' | '새 칼럼 생성';
+  onOkClick: () => void;
+  onCancelClick: () => void;
 }
 
-function Basic({ description }: Props) {
+function Basic({ onCancelClick, onOkClick, type }: Props) {
   return (
-    <StyledContainer>
-      <StyledDescription>{description}</StyledDescription>;
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        {type === '초대하기' && <BasicInput label="이메일" />}
+        {type === '새 칼럼 생성' && <BasicInput label="이름" placeholder="새로운 프로젝트" />}
+      </StyledContainer>
+
+      <StyledButtonContainer>
+        <StyledTwinButton
+          text1="취소"
+          text2={type === '초대하기' ? '초대' : '생성'}
+          size="large"
+          onLeftClick={onCancelClick}
+          onRightClick={onOkClick}
+        ></StyledTwinButton>
+      </StyledButtonContainer>
+    </>
   );
 }
 
 export default Basic;
 
 const StyledContainer = styled.div`
-  /* padding: 100px 160px; */
   white-space: nowrap;
 `;
 
-const StyledDescription = styled.h5`
-  ${fontStyle(18, 500)}
-  text-align: center
+const StyledButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 38.4px;
+
+  ${onMobile} {
+    justify-content: center;
+  }
+`;
+
+const StyledTwinButton = styled(TwinButton)`
+  & > button {
+    border-radius: 0.5rem;
+  }
 `;
