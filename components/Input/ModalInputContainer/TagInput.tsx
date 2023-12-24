fceeaeea76ad6ce9958302d77d3bf19134c3d028
arky02 <kyean07@naver.com>
2023-12-25 03:42:57 +0900

    [#41] Fix: TagInput 에러처리 기능 제거

--- components/Input/ModalInputContainer/TagInput.tsx
+++ components/Input/ModalInputContainer/TagInput.tsx
@@ -2,15 +2,12 @@ import styled from 'styled-components';
 import { ChangeEvent, useState, MouseEvent, KeyboardEvent, useEffect } from 'react';
 import { fontStyle } from '@/styles/fontStyle';
 import { COLORS } from '@/styles/palettes';
-import { NO_VALUE_ERROR } from '@/constants/Input';
 import { TAG_COLOR } from '@/constants/Input';
-import { StyledErrorText, StyledLabel } from '../Input.style';
+import { StyledLabel } from '../Input.style';
 import ContentChip from '@/components/Chip/ContentChip';
 
 export interface TagProps {
   inputValue?: string;
-  placeholder?: string;
-  errorMessage?: string;
   label?: string;
   onChange: (inputLabel: string, value: Tag[]) => void;
   onButtonClick?: (e: MouseEvent<HTMLElement>) => void;
@@ -23,16 +20,12 @@ interface Tag {
 }
 /**
  * Modal Tag Input
- * @param errorMessage 부모 컴포넌트에서 제어하는 input에 띄우고자 하는 에러 메세지, exist하면 error design 표시
  * @param label input 라벨 텍스트
  * @param onChange 부모 컴포넌트에서 제어하는 input onChange 함수
  * */
-function TagInput({ errorMessage = '', label = '태그', onChange }: TagProps) {
-  const [isNoValue, setIsNoValue] = useState<boolean>(false);
+function TagInput({ label = '태그', onChange }: TagProps) {
   const [tags, setTags] = useState<Tag[]>([]);
   const [value, setValue] = useState<string>('');
-
-  const hasError = errorMessage !== '';
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -55,15 +48,6 @@ function TagInput({ errorMessage = '', label = '태그', onChange }: TagProps) {
     });
 
     setValue('');
-    checkEmptyValError(newTagEl.value);
-  };
-
-  const handleBlur = () => {
-    checkEmptyValError();
-  };
-
-  const checkEmptyValError = (isNewTagExist: string = '') => {
-    !isNewTagExist && tags.length === 0 ? setIsNoValue(true) : setIsNoValue(false);
   };
 
   useEffect(() => {
@@ -73,7 +57,7 @@ function TagInput({ errorMessage = '', label = '태그', onChange }: TagProps) {
   return (
     <div>
       <StyledLabel>{label}</StyledLabel>
-      <StyledInputContainer $error={isNoValue || hasError} onBlur={handleBlur}>
+      <StyledInputContainer>
         {tags &&
           tags.map((value, index) => (
             <ContentChip text={value.value} key={index} backgroundColor={value.backgroundColor} color={value.color}>
@@ -96,7 +80,6 @@ function TagInput({ errorMessage = '', label = '태그', onChange }: TagProps) {
           />
         )}
       </StyledInputContainer>
-      {(isNoValue || hasError) && <StyledErrorText>{errorMessage || NO_VALUE_ERROR}</StyledErrorText>}
     </div>
   );
 }
@@ -113,11 +96,11 @@ const StyledInput = styled.input`
   padding: 13px 10px 15px;
 `;
 
-const StyledInputContainer = styled.div<{ $error: boolean }>`
+const StyledInputContainer = styled.div`
   width: 100%;
   padding-left: 5px;
   border-radius: 8px;
-  border: 1px solid ${({ $error }) => ($error ? COLORS.RED_D6 : COLORS.GRAY_D9)};
+  border: 1px solid ${COLORS.GRAY_D9};
   color: ${COLORS.BLACK_33};
   ${fontStyle(16, 400)};
   overflow-x: scroll;
@@ -129,7 +112,7 @@ const StyledInputContainer = styled.div<{ $error: boolean }>`
   &:hover,
   &:focus,
   &:active {
-    border: 1px solid ${({ $error }) => ($error ? COLORS.RED_D6 : COLORS.VIOLET_55)};
+    border: 1px solid ${COLORS.VIOLET_55};
     color: ${COLORS.BLACK_33};
     outline: none;
   }
