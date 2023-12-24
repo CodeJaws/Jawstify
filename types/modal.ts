94c10a5113e4739022715582c09a5876c86be3b5
arky02 <kyean07@naver.com>
2023-12-25 03:04:24 +0900

    [#41] Feat: modal.ts 생성

--- types/modal.ts
+++ types/modal.ts
@@ -1,4 +1,8 @@
+import { ChangeEvent } from 'react';
+
 export interface ModalOnClickProps {
   onOkClick: () => void;
   onCancelClick: () => void;
 }
+
+export type InputChangeType = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
