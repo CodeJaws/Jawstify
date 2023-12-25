0f1cc1e480eff885394397fff48bed985b75420f
박경서 <zerm0907@gmail.com>
2023-12-25 23:42:31 +0900

    [#60] Feat: 어찌저찌 우당탕탕 완성된 페이지네이션

--- components/MyDashboard/MyDashBoardButtonBox.tsx
+++ components/MyDashboard/MyDashBoardButtonBox.tsx
@@ -3,39 +3,68 @@ import styled from 'styled-components';
 import DashBoardAddButton from '../common/Button/DashBoardAddButton';
 import DashBoardButton from '../common/Button/DashBoardButton';
 import PaginationButton from '../common/Button/PaginationButton';
-import usePagination from '@/hooks/usePagination';
 import { COLORS } from '@/styles/palettes';
 import { fontStyle } from '@/styles/fontStyle';
+import { useState } from 'react';
 
-interface MyDashBoardButtonBoxProps {
-  dashboardId: number;
-}
+const mock = [
+  { id: 1, text: '일' },
+  { id: 2, text: '이' },
+  { id: 3, text: '삼' },
+  { id: 4, text: '사' },
+  { id: 5, text: '오' },
+  { id: 6, text: '육' },
+  { id: 7, text: '칠' },
+  { id: 8, text: '팔' },
+  { id: 9, text: '구' },
+  { id: 10, text: '십' },
+  { id: 11, text: '십일' },
+  { id: 12, text: '십이' },
+];
 
-function MyDashBoardButtonBox({ dashboardId }: MyDashBoardButtonBoxProps) {
-  const { handlePagination, pageNum, showItems, totalPages, totalCount } = usePagination({
-    size: 20,
-    showItemNum: 4,
-    type: 'members',
-    dashboardId,
-  });
+function MyDashBoardButtonBox() {
+  const [page, setPage] = useState(1);
+  const limit = 5;
+  const firstIndex = (page - 1) * limit;
+  const lastIndex = firstIndex + limit;
+  const currentItem = mock.slice(firstIndex, lastIndex);
+
+  const totalItemCount = Object.keys(mock).length;
+  const lastPages = Math.ceil(totalItemCount / 5);
+
+  const handleLastPage = () => {
+    if (page === 1) {
+      return;
+    } else {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (page === lastPages) {
+      return;
+    } else {
+      setPage((prev) => prev + 1);
+    }
+  };
 
   return (
     <div>
       <ButtonBoxWrapper>
         <DashBoardAddButton onClick={() => {}} />
-        <DashBoardButton text="비브리지" color="dd" king={true} onClick={() => {}} />
-        <DashBoardButton text="비브리지" color="dd" king={true} onClick={() => {}} />
-        <DashBoardButton text="비브리지" color="dd" king={true} onClick={() => {}} />
-        <DashBoardButton text="비브리지" color="dd" king={true} onClick={() => {}} />
-        <DashBoardButton text="비브리지" color="dd" king={true} onClick={() => {}} />
+        {currentItem.map((item) => (
+          <div key={item.id}>
+            <DashBoardButton text={item.text} color="" king={true} onClick={() => {}} />
+          </div>
+        ))}
       </ButtonBoxWrapper>
       <PaginationWrapper>
         <PaginationPage>
-          {Math.ceil(totalCount / 4)} 페이지 중 {pageNum}
+          {Math.ceil(totalItemCount / 4)} 페이지 중 {page}
         </PaginationPage>
         <PaginationInWrapper>
-          <PaginationButton active={true} direction="left" onClick={() => {}} />
-          <PaginationButton active={true} direction="right" onClick={() => {}} />
+          <PaginationButton active={page !== 1} direction="left" onClick={handleLastPage} />
+          <PaginationButton active={page !== lastPages} direction="right" onClick={handleNextPage} />
         </PaginationInWrapper>
       </PaginationWrapper>
     </div>
@@ -56,13 +85,13 @@ const ButtonBoxWrapper = styled.div`
     grid-template-columns: 1fr 1fr;
 
     width: 504px;
-    height: 280px;
+    height: 100%;
   }
   ${onMobile} {
     padding-bottom: 8px;
     grid-template-columns: 1fr;
     width: 260px;
-    height: 435px;
+    height: 100%;
   }
 `;
 
