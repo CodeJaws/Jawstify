0f4c9c237e6642017ada86a3f4fc193f291dac21
Soeun Kim <summerkim0825@gmail.com>
2023-12-26 18:11:35 +0900

    [#80] Feat: sidebar, dashboardbar 컴포넌트 가져오기

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,17 @@
-function DashBoard() {
-  return <div>DashBoard page</div>;
+import DashboardNavbar from '@/components/DashboardNavbar/DashboardNavbar';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import { useRouter } from 'next/router';
+
+function DashBoardID() {
+  const router = useRouter();
+  const { dashboardid: id } = router.query;
+
+  return (
+    <>
+      <Sidebar />
+      <DashboardNavbar isMyDashboard={false} isOwner={true} title='비브리지' />
+    </>
+  );
 }
 
-export default DashBoard;
+export default DashBoardID;
