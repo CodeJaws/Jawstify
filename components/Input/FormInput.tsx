d7459ec26713e761fd4d7094bfb9821f84ee9477
arky02 <kyean07@naver.com>
2023-12-26 16:30:09 +0900

    [#74] Refactor: FormInput 자체 에러 검증 기능 삭제

--- components/Input/FormInput.tsx
+++ components/Input/FormInput.tsx
@@ -5,6 +5,7 @@ import eyeOn from '@/public/assets/icons/eyeon.svg';
 import eyeOff from '@/public/assets/icons/eyeoff.svg';
 import { DEFAULT_PLACEHOLDER, NO_VALUE_ERROR } from '@/constants/Input';
 import { StyledInputContainer, StyledLabel, StyledInput, StyledErrorText } from './Input.style';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 // 아래 4개의 이외의 경우 Input 사용 시 Basic Input 사용
 // Login, SignForm에 쓰이는 Input들
@@ -28,9 +29,10 @@ interface Props {
   inputValue?: string;
   placeholder?: string;
   errorMessage?: string;
+  register: UseFormRegisterReturn;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
-function FormInput({ label = '', inputValue = '', placeholder, errorMessage = '', onChange }: Props) {
+function FormInput({ label = '', inputValue = '', placeholder, errorMessage = '', register, onChange }: Props) {
   const [isNoValue, setIsNoValue] = useState<boolean>(false);
   const [isVisible, setIsVisible] = useState(true);
 
@@ -48,6 +50,7 @@ function FormInput({ label = '', inputValue = '', placeholder, errorMessage = ''
         value={inputValue}
         placeholder={placeholder || PLACEHOLDER[label]}
         $error={isNoValue || hasError}
+        {...register}
         onChange={onChange}
         onBlur={handleBlur}
       />
