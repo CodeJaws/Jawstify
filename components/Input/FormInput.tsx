b84b6b69912887c67c1fdec6d433f01fe100f6da
arky02 <kyean07@naver.com>
2023-12-26 16:25:47 +0900

    Revert "[#74] Refactor: FormInput에서 자체적으로 형식 에러 띄울 수 있도록 수정"
    This reverts commit 7a163326dec24900fde328fe73fcff253aea6790.

--- components/Input/FormInput.tsx
+++ components/Input/FormInput.tsx
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import eyeOn from '@/public/assets/icons/eyeon.svg';
 import eyeOff from '@/public/assets/icons/eyeoff.svg';
-import { isEmailFormatValid, isPwdFormatValid } from '@/utils/validation';
-import { DEFAULT_PLACEHOLDER, EMAIL_ERROR, NO_VALUE_ERROR, PWD_ERROR } from '@/constants/Input';
+import { DEFAULT_PLACEHOLDER, NO_VALUE_ERROR } from '@/constants/Input';
 import { StyledInputContainer, StyledLabel, StyledInput, StyledErrorText } from './Input.style';
-import { UseFormRegisterReturn } from 'react-hook-form';
 
+// 아래 4개의 이외의 경우 Input 사용 시 Basic Input 사용
+// Login, SignForm에 쓰이는 Input들
 const PLACEHOLDER = {
   이메일: DEFAULT_PLACEHOLDER.EMAIL,
   닉네임: DEFAULT_PLACEHOLDER.NICKNAME,
@@ -15,54 +15,40 @@ const PLACEHOLDER = {
   '비밀번호 확인': DEFAULT_PLACEHOLDER.PWD_CHECK,
   '': '',
 };
+
+/**
+ * @param label input 라벨 텍스트
+ * @param inputValue 부모 컴포넌트에서 제어하는 input 텍스트 값
+ * @param placeholder input placeholder 텍스트
+ * @param errorMessage 부모 컴포넌트에서 제어하는 input에 띄우고자 하는 에러 메세지, exist하면 error design 표시
+ * @param onChange 부모 컴포넌트에서 제어하는 input onChange 함수
+ * */
 interface Props {
-  placeholder?: any;
-  errorMessage?: string;
-  label?: keyof typeof PLACEHOLDER;
+  label: keyof typeof PLACEHOLDER;
   inputValue?: string;
-  type?: string;
-  register: UseFormRegisterReturn;
+  placeholder?: string;
+  errorMessage?: string;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
-function Input({ placeholder, errorMessage = '', label = '', inputValue, register, onChange }: Props) {
-  const [errorMsg, setErrorMsg] = useState(errorMessage);
-  const [isNoVal, setIsNoVal] = useState<boolean>(false);
-  const [isVisible, setIsVisible] = useState(false);
-  const [value, setValue] = useState('');
+function FormInput({ label = '', inputValue = '', placeholder, errorMessage = '', onChange }: Props) {
+  const [isNoValue, setIsNoValue] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState(true);
 
   const isPassword = label.includes('비밀번호');
-  const isEmail = label === '이메일';
+  const hasError = errorMessage !== '';
+
   const handleVisibility = () => setIsVisible((prev) => !prev);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const currentVal = e.target.value;
-    setValue(currentVal);
-    !currentVal ? setIsNoVal(true) : setIsNoVal(false);
-    handleErrorState(currentVal, isPassword);
-  };
-
-  const handleErrorState = (inputValue: string, isPassword: boolean) => {
-    if (isPassword) {
-      isPwdFormatValid(inputValue) ? setErrorMsg('') : setErrorMsg(PWD_ERROR.FORMAT_ERROR);
-    } else if (isEmail) {
-      isEmailFormatValid(inputValue) ? setErrorMsg('') : setErrorMsg(EMAIL_ERROR.FORMAT_ERROR);
-    }
-  };
-
-  const handleBlur = () => {
-    !value ? setIsNoVal(true) : setIsNoVal(false);
-  };
-
+  const handleBlur = () => (inputValue === '' ? setIsNoValue(true) : setIsNoValue(false));
   return (
     <StyledInputContainer>
       <StyledLabel>{label}</StyledLabel>
       <StyledInput
-        type={!isVisible && isPassword ? 'password' : 'text'}
-        $error={isNoVal || Boolean(errorMsg)}
+        type={isVisible ? 'text' : 'password'}
+        value={inputValue}
         placeholder={placeholder || PLACEHOLDER[label]}
-        value={value}
-        {...register}
-        onChange={handleChange}
+        $error={isNoValue || hasError}
+        onChange={onChange}
         onBlur={handleBlur}
       />
       {isPassword && (
@@ -70,12 +56,12 @@ function Input({ placeholder, errorMessage = '', label = '', inputValue, registe
           <StyledImage src={isVisible ? eyeOn : eyeOff} width={24} height={24} alt="비밀번호 숨기기" />
         </StyledEyeButton>
       )}
-      {(isNoVal || errorMsg) && <StyledErrorText>{errorMsg || NO_VALUE_ERROR}</StyledErrorText>}
+      {(isNoValue || hasError) && <StyledErrorText>{errorMessage || NO_VALUE_ERROR}</StyledErrorText>}
     </StyledInputContainer>
   );
 }
 
-export default Input;
+export default FormInput;
 
 const StyledImage = styled(Image)`
   border: none;
