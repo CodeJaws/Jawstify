9d64945489e9d0aa6168f600828a31159fa4fc2a
oauch <lgm9515@naver.com>
2023-12-26 20:37:14 +0900

    [#76] Feat: PasswordManagerBox 컴포넌트 API 연동

--- components/MyPage/PasswordManagerBox.tsx
+++ components/MyPage/PasswordManagerBox.tsx
@@ -1,25 +1,96 @@
+import api from '@/apis/api';
+import { EQUAL_NOW_PWD, NEW_PWD_EIGHT, NEW_PWD_INPUT, NOW_PWD_NOT_MATCH, PWD_EIGHT } from '@/constants/ErrorMsg';
 import { fontStyle } from '@/styles/fontStyle';
 import { onMobile, onTablet } from '@/styles/mediaQuery';
 import { COLORS } from '@/styles/palettes';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { styled } from 'styled-components';
-import BasicInput from '../Input/ModalInputContainer/BasicInput';
+import FormInput from '../Input/FormInput';
 import Button from '../common/Button/Button';
 
-function PasswordManagerBox() {
-  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
-
-  const handleSave = () => {
-    console.log('버튼 눌림');
+interface FormProps {
+  password: string;
+  newPassword: string;
+  newPasswordCheck: string;
+  errors: {
+    password: {
+      message: string;
+    };
+    newPassword: {
+      message: string;
+    };
+    newPasswordCheck: {
+      message: string;
+    };
   };
+}
+
+function PasswordManagerBox() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setError,
+    formState: { errors },
+  } = useForm<FormProps>({ mode: 'onBlur' });
+  const [isActivate, setActivate] = useState(true);
+
+  const onSubmit = async (formValues: any) => {
+    const password = formValues.password;
+    const newPassword = formValues.newPassword;
+    try {
+      await api.auth.changePassword({ password, newPassword });
+    } catch (error: any) {
+      switch (error.data.message) {
+        case PWD_EIGHT:
+          setError('password', { message: PWD_EIGHT });
+          break;
+        case NOW_PWD_NOT_MATCH:
+          setError('password', { message: NOW_PWD_NOT_MATCH });
+          break;
+        case NEW_PWD_EIGHT:
+          setError('newPassword', { message: NEW_PWD_EIGHT });
+          break;
+        case NEW_PWD_INPUT:
+          setError('newPassword', { message: NEW_PWD_INPUT });
+          break;
+        case EQUAL_NOW_PWD:
+          setError('newPassword', { message: EQUAL_NOW_PWD });
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const passwordRegister = register('password');
+  const newPasswordRegister = register('newPassword');
+  const newPasswordCheckRegister = register('newPasswordCheck', {
+    validate: (value, formValues) => {
+      return value === formValues.newPassword || '비밀번호가 일치하지않습니다';
+    },
+  });
+
+  const handleCheck = () => {
+    const password = getValues('password');
+    const newPassword = getValues('newPassword');
+    const newPasswordCheck = getValues('newPasswordCheck');
+    if (password && newPassword && newPasswordCheck) setActivate(false);
+  };
+
   return (
     <StyledContainer>
       <StyledTitle>비밀번호 변경</StyledTitle>
-      <StyledWrapper>
-        <BasicInput label="현재 비밀번호" placeholder="현재 비밀번호 입력" />
-        <BasicInput label="새 비밀번호" placeholder="새 비밀번호 입력" />
-        <BasicInput label="새 비밀번호 확인" placeholder="새 비밀번호 확인 입력" />
-        <StyledButton text={'변경'} size={'small'} isViolet={true} onClick={handleSave} className={''} />
+      <StyledWrapper onSubmit={handleSubmit(onSubmit)} onChange={handleCheck}>
+        <FormInput register={passwordRegister} label="현재 비밀번호" errorMessage={errors.password?.message} />
+        <FormInput register={newPasswordRegister} label="새 비밀번호" errorMessage={errors.newPassword?.message} />
+        <FormInput
+          register={newPasswordCheckRegister}
+          label="새 비밀번호 확인"
+          errorMessage={errors.newPasswordCheck?.message}
+        />
+        <StyledButton text={'변경'} size={'small'} isViolet={true} disabled={isActivate} />
       </StyledWrapper>
     </StyledContainer>
   );
@@ -31,7 +102,7 @@ const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 620px;
-  height: 454px;
+  height: auto;
   margin-top: 12px;
   gap: 32px;
   padding: 32px 28px 28px 28px;
@@ -56,17 +127,24 @@ const StyledTitle = styled.p`
   }
 `;
 
-const StyledWrapper = styled.div`
+const StyledWrapper = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: end;
+  align-items: flex-start;
   gap: 20px;
 `;
 
 const StyledButton = styled(Button)`
+  margin-left: auto;
+
   ${onMobile} {
     width: 84px;
     gap: 16px;
   }
+`;
+
+const StyledErrorMsg = styled.p`
+  color: ${COLORS.RED_D6};
+  ${fontStyle(14, 400)};
 `;
