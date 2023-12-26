95e866b3aa41d92dd6b39ab1e0dd0b52dcc85d62
oauch <lgm9515@naver.com>
2023-12-26 20:33:23 +0900

    [#76] Feat: Input, ErrorMsg 상수화

---
+++ constants/ErrorMsg.ts
@@ -0,0 +1,5 @@
+export const PWD_EIGHT = '비밀번호는 8자 이상 입력해주세요.';
+export const NOW_PWD_NOT_MATCH = '현재 비밀번호가 틀렸습니다.';
+export const NEW_PWD_EIGHT = '새 비밀번호는 8자 이상 입력해주세요.';
+export const NEW_PWD_INPUT = '새 비밀번호를 입력해주세요.';
+export const EQUAL_NOW_PWD = '기존 비밀번호와 동일합니다.';
