5c60f44a081ff2dd04cde89db80a3483e2f4d8ae
qooktree1 <qooktree@gmail.com>
2023-12-28 00:09:57 +0900

    [#84] Feat: 모달 초대 에러 메세지 텍스트 저장

---
+++ constants/Modal.ts
@@ -0,0 +1,7 @@
+export const INVITE_ERROR = {
+  EMAIL_ERROR: '이메일 형식이 올바르지 않습니다.',
+  INVITE_AUTH_ERROR: '대시보드 초대 권한이 없습니다.',
+  NO_DASHBOARD_ERROR: '대시보드가 존재하지 않습니다.',
+  NO_USER_ERROR: '존재하지 않는 유저입니다.',
+  ALREADY_INVITE_ERROR: '이미 대시보드에 초대된 멤버입니다.',
+};
