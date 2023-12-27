c141b5e523e9c4a3bcf312e9fb5d936da04194b1
oauch <lgm9515@naver.com>
2023-12-27 17:02:52 +0900

    [#76] Feat: ProfileBox API 연결

--- components/MyPage/ProfileBox.tsx
+++ components/MyPage/ProfileBox.tsx
@@ -1,23 +1,63 @@
+import API from '@/apis/api';
 import { onMobile, onTablet } from '@/styles/mediaQuery';
 import { COLORS } from '@/styles/palettes';
-import { useState } from 'react';
+import axios from 'axios';
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 import { styled } from 'styled-components';
 import AddImageButton from '../AddImageButton/AddImageButton';
-import FormInput from '../Input/FormInput';
+import BasicInput from '../Input/ModalInputContainer/BasicInput';
 import Button from '../common/Button/Button';
 
-function ProfileBox() {
-  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
-  const handleSave = () => {
-    return;
+interface Props {
+  email: string;
+  nickname: string;
+  profileImg: string;
+  setNickName: Dispatch<SetStateAction<string>>;
+}
+
+function ProfileBox({ email, nickname, profileImg, setNickName }: Props) {
+  const [image, setImage] = useState<File>();
+  const [previewImage, setPreviewImage] = useState<string | ArrayBuffer | null>(null);
+
+  const changeNickName = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setNickName(e.target.value);
   };
+
+  const changeProfile = async () => {
+    if (!image) {
+      const profileImageUrl = profileImg;
+      await API.users.correctMyInfo({ profileImageUrl, nickname }).then(() => alert('프로필 업데이트 완료 🍀'));
+      return;
+    } else {
+      const formData = new FormData();
+      formData.append('image', image);
+
+      const response = await axios.post('https://sp-taskify-api.vercel.app/1-4/users/me/image', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+          withCredentials: true,
+        },
+      });
+      const { profileImageUrl } = response.data;
+
+      await API.users.correctMyInfo({ profileImageUrl, nickname }).then(() => alert('프로필 업데이트 완료 🍀'));
+    }
+  };
+
   return (
     <StyledContainer>
-      <AddImageButton type={'profile'} image={image} setImage={setImage} />
+      <AddImageButton
+        type={'profile'}
+        profileImg={profileImg}
+        previewImage={previewImage}
+        setPreviewImage={setPreviewImage}
+        setImage={setImage}
+      />
       <StyledWrapper>
-        <FormInput label="이메일" />
-        <FormInput label="닉네임" />
-        <StyledButton text={'저장'} size={'small'} isViolet={true} onClick={handleSave} className={''} />
+        <BasicInput label="이메일" inputValue={email} disabled />
+        <BasicInput label="닉네임" inputValue={nickname} onChange={changeNickName} />
+        <StyledButton text={'저장'} size={'small'} isViolet={true} onClick={changeProfile} />
       </StyledWrapper>
     </StyledContainer>
   );
@@ -49,8 +89,8 @@ const StyledContainer = styled.div`
 const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: end;
+  justify-content: flex-end;
+  align-items: flex-end;
   width: 100%;
   gap: 20px;
 
