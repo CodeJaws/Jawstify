8ebf845e54adde3654325520f1e56a16c1b66886
oauch <lgm9515@naver.com>
2023-12-27 18:32:17 +0900

    [#76] Feat: 프로필 변경 에러메시지 상수화

--- constants/ErrorMsg.ts
+++ constants/ErrorMsg.ts
@@ -1,5 +1,8 @@
-export const PWD_EIGHT = '비밀번호는 8자 이상 입력해주세요.';
-export const NOW_PWD_NOT_MATCH = '현재 비밀번호가 틀렸습니다.';
-export const NEW_PWD_EIGHT = '새 비밀번호는 8자 이상 입력해주세요.';
-export const NEW_PWD_INPUT = '새 비밀번호를 입력해주세요.';
-export const EQUAL_NOW_PWD = '기존 비밀번호와 동일합니다.';
+export const PWD_EIGHT_ERROR = '비밀번호는 8자 이상 입력해주세요.';
+export const NOW_PWD_NOT_MATCH_ERROR = '현재 비밀번호가 틀렸습니다.';
+export const NEW_PWD_EIGHT_ERROR = '새 비밀번호는 8자 이상 입력해주세요.';
+export const NEW_PWD_INPUT_ERROR = '새 비밀번호를 입력해주세요.';
+export const EQUAL_NOW_PWD_ERROR = '기존 비밀번호와 동일합니다.';
+export const NICKNAME_ERROR = '닉네임은 10자 이하로 작성해주세요.';
+export const NICKNAME_IMG_ERROR = '닉네임을 입력해주세요.';
+export const IMG_URL_ERROR = '프로필 이미지 URL 형식이 올바르지 않습니다.';
