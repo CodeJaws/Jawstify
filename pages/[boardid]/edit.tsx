26912ec6d0a5da5aa26392e6d52ec996ae353d01
qooktree1 <qooktree@gmail.com>
2023-12-29 21:44:22 +0900

    [#67] Feat: 대시보드 수정 페이지 구현

--- pages/[boardid]/edit.tsx
+++ pages/[boardid]/edit.tsx
@@ -1,5 +1,143 @@
-function BoardEdit() {
-  return <div>BoardEdit page</div>;
+import Link from 'next/link';
+import styled from 'styled-components';
+import { GetServerSideProps } from 'next';
+
+import { COLORS } from '@/styles/palettes';
+import { fontStyle } from '@/styles/fontStyle';
+import { onMobile, onTablet } from '@/styles/mediaQuery';
+import DashboardNavbar from '@/components/DashboardNavbar/DashboardNavbar';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import BackImg from '@/public/assets/icons/LeftArrow.svg';
+import { useRouter } from 'next/router';
+import DashboardEdit from '@/components/Edit/DashboardEdit';
+import useDashboard from '@/hooks/useDashboard';
+import API from '@/apis/api';
+import MembersTable from '@/components/Table/MembersTable';
+import InviteDetailsTable from '@/components/Table/InviteDetailsTable';
+import { useEffect, useState } from 'react';
+
+interface BoardEditProps {
+  boardid: number;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { boardid } = context.query;
+
+  if (!boardid) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      boardid,
+    },
+  };
+};
+
+function BoardEdit({ boardid: dashboardId }: BoardEditProps) {
+  const router = useRouter();
+  const [refreshToggle, setRefreshToggle] = useState(false);
+  const { members, totalMembers, dashboardData } = useDashboard({ dashboardId, refreshToggle });
+
+  const refresh = () => setRefreshToggle((prev) => !prev);
+
+  const deleteDashboard = async () => {
+    if (confirm('정말 대시보드를 삭제하시겠습니까?')) {
+      try {
+        await API.dashboard.deleteDashboard({ dashboardId: String(dashboardId) });
+        router.push('/mydashboard');
+      } catch (e: any) {
+        switch (e.data.message) {
+          case '대시보드 삭제 권한이 없습니다.':
+            alert('대시보드 삭제 권한이 없습니다.');
+            break;
+          case '대시보드가 존재하지 않습니다.':
+            alert('대시보드가 존재하지 않습니다.');
+            break;
+        }
+      }
+    }
+  };
+
+  return (
+    <StyledContainer>
+      <DashboardNavbar members={members} totalMembers={totalMembers} dashboard={dashboardData} isMyDashboard={false} />
+      <Sidebar />
+      <StyledWrapper>
+        <StyledInWrapper>
+          <StyledLink href={`/dashboard/${dashboardId}`}>돌아가기</StyledLink>
+          <StyledMainWrapper>
+            <DashboardEdit dashboardData={dashboardData} refresh={refresh} />
+            <div style={{ margin: '12px 0' }}>
+              <MembersTable dashboardId={Number(dashboardId)} refresh={refresh} />
+            </div>
+            <InviteDetailsTable dashboardId={Number(dashboardId)} />
+            <StyledDeleteButton onClick={deleteDashboard}>대시보드 삭제하기</StyledDeleteButton>
+          </StyledMainWrapper>
+        </StyledInWrapper>
+      </StyledWrapper>
+    </StyledContainer>
+  );
 }
 
 export default BoardEdit;
+
+const StyledContainer = styled.div`
+  width: 100%;
+`;
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 1080px;
+`;
+
+const StyledInWrapper = styled.div`
+  position: absolute;
+  top: 70px;
+  left: 300px;
+  width: calc(100% - 300px);
+  height: 100vh;
+  padding: 20px;
+
+  ${onTablet} {
+    left: 160px;
+    width: calc(100% - 160px);
+  }
+  ${onMobile} {
+    left: 66px;
+    width: calc(100% - 66px);
+  }
+`;
+
+const StyledLink = styled(Link)`
+  ${fontStyle(16, 500)}
+  background-image: url(${BackImg.src});
+  background-repeat: no-repeat;
+  background-position: 0px 50%;
+  padding-left: 20px;
+`;
+
+const StyledMainWrapper = styled.div`
+  margin-top: 25px;
+  gap: 12px;
+`;
+
+const StyledDeleteButton = styled.button`
+  margin-top: 40px;
+  display: flex;
+  width: 320px;
+  height: 62px;
+  /* padding: 20px 95px; */
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  border: 1px solid ${COLORS.GRAY_D9};
+  background: ${COLORS.GRAY_FA};
+
+  color: ${COLORS.BLACK_33};
+  ${fontStyle(18, 500)};
+  text-align: center;
+`;
