574f68f0845b53b874653d700241ab2da2a75ffe
qooktree1 <qooktree@gmail.com>
2023-12-29 21:45:14 +0900

    [#67] Feat: 유저 데이터 받은 수 있는 custom hook 구현

---
+++ hooks/global/useUserData.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+
+import { persist } from 'zustand/middleware';
+import { UserType } from '@/types/apiType';
+
+interface useUserDataProps {
+  user: UserType;
+  setUser: (value: UserType) => void;
+}
+
+const useUserData = create(
+  persist<useUserDataProps>(
+    (set, get) => ({
+      user: {
+        createdAt: '',
+        email: '',
+        id: 0,
+        nickname: '',
+        profileImageUrl: '',
+        updatedAt: '',
+      },
+      setUser: (value: UserType) => set({ user: value }),
+    }),
+    {
+      name: 'userDataStorage',
+    },
+  ),
+);
+
+export default useUserData;
