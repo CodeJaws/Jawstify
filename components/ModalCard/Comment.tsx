5a7c71624d1e792d29e585a0adff76f6b80f754d
oauch <lgm9515@naver.com>
2023-12-31 01:17:31 +0900

    [#97] Feat: Comment 컴포넌트 API 연결

--- components/ModalCard/Comment.tsx
+++ components/ModalCard/Comment.tsx
@@ -1,35 +1,114 @@
+import API from '@/apis/api';
+import useCardData from '@/hooks/ModalCard/useCardData';
+import useCardId from '@/hooks/ModalCard/useCardId';
 import useComment from '@/hooks/ModalCard/useComment';
+import useRefresh from '@/hooks/useRefresh';
+import Emoji from '@/public/assets/images/emoji.webp';
 import { fontStyle } from '@/styles/fontStyle';
 import { onMobile } from '@/styles/mediaQuery';
 import { COLORS } from '@/styles/palettes';
 import dateTimeFormat from '@/utils/dateTimeFormat';
 import Image from 'next/image';
+import { ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
 import BasicInput from '../Input/ModalInputContainer/BasicInput';
 
 function Comment() {
   const { comment } = useComment();
+  const { cardData } = useCardData();
+  const { cardId } = useCardId();
+  const { refresh, setRefresh } = useRefresh();
+
+  // 댓글 별로 수정 여부를 관리하는 상태 추가
+  const [isUpdateMap, setIsUpdateMap] = useState<{ [key: number]: boolean }>({});
+  const [updatedCommentMap, setUpdatedCommentMap] = useState<{ [key: number]: string }>({});
+  const [value, setValues] = useState('');
+
+  const isOpenComment = (commentId: number) => {
+    setIsUpdateMap((prev) => ({ ...prev, [commentId]: !prev[commentId] }));
+  };
+
+  const handleUpdateInputChange = (commentId: number, e: ChangeEvent<HTMLTextAreaElement>) => {
+    setUpdatedCommentMap((prev) => ({ ...prev, [commentId]: e.target.value }));
+  };
+
+  const handleUpdateButtonClick = async (commentId: number) => {
+    const content = updatedCommentMap[commentId];
+    setIsUpdateMap((prev) => ({ ...prev, [commentId]: false }));
+    await API.comments.correctComment({ commentId, content });
+    setRefresh(!refresh);
+  };
+
+  const handleChange = (e: string) => {
+    setValues(e);
+  };
+
+  const submitComment = async () => {
+    const body = {
+      content: value,
+      cardId: cardId,
+      columnId: cardData.columnId,
+      dashboardId: 325, // @TODO 대시보드 아이디 가져오기
+    };
+
+    await API.comments.createComment(body).then(() => alert('댓글 보내기 성공!'));
+    setRefresh(!refresh);
+    setValues('');
+  };
+
+  const deleteComment = async (commentId: number) => {
+    await API.comments.deleteComment({ commentId }).then(() => alert('❌ 댓글 삭제 성공!'));
+    setRefresh(!refresh);
+  };
 
   return (
     <StyledContainer>
-      <BasicInput onChange={() => {}} label="댓글" />
+      <BasicInput
+        label="댓글"
+        inputValue={value}
+        onChange={(label, value) => {
+          handleChange(value);
+        }}
+        onButtonClick={() => {
+          submitComment();
+        }}
+      />
       {comment.comments.map((val) => (
-        <>
-          <StyledInCommentWrapper>
+        <StyledInCommentWrapper key={val.id}>
+          {val.author.profileImageUrl ? (
             <StyledImage width={34} height={34} src={val.author.profileImageUrl} alt="프로필 이미지" />
-            <StyledCommentContent>
-              <StyledInComment>
-                <StyledUser>{val.author.nickname}</StyledUser>
-                <StyledDate>{dateTimeFormat(val.createdAt)}</StyledDate>
-              </StyledInComment>
+          ) : (
+            <StyledImage width={34} height={34} src={Emoji} alt="프로필 이미지" />
+          )}
+
+          <StyledCommentContent>
+            <StyledInComment>
+              <StyledUser>{val.author.nickname}</StyledUser>
+              <StyledDate>{dateTimeFormat(val.createdAt)}</StyledDate>
+            </StyledInComment>
+            {isUpdateMap[val.id] ? (
+              <StyledInputWrapper>
+                <StyledInput
+                  value={updatedCommentMap[val.id] || val.content}
+                  onChange={(e) => handleUpdateInputChange(val.id, e)}
+                  placeholder="수정할 내용을 입력하세요."
+                />
+                <StyledButtonInWrapper>
+                  <StyledButton onClick={() => handleUpdateButtonClick(val.id)}>완료</StyledButton>
+                  <StyledButton onClick={() => isOpenComment(val.id)}>취소</StyledButton>
+                </StyledButtonInWrapper>
+              </StyledInputWrapper>
+            ) : (
               <StyledComment>{val.content}</StyledComment>
+            )}
+            {!isUpdateMap[val.id] && (
               <StyledButtonWrapper>
-                <StyledButton>수정</StyledButton>
-                <StyledButton>삭제</StyledButton>
+                <StyledButton onClick={() => isOpenComment(val.id)}>수정</StyledButton>
+                <StyledButton onClick={() => deleteComment(val.id)}>삭제</StyledButton>
               </StyledButtonWrapper>
-            </StyledCommentContent>
-          </StyledInCommentWrapper>
-        </>
+            )}
+          </StyledCommentContent>
+        </StyledInCommentWrapper>
       ))}
     </StyledContainer>
   );
@@ -60,10 +139,21 @@ const StyledImage = styled(Image)`
 const StyledCommentContent = styled.div`
   display: flex;
   flex-direction: column;
+  flex-grow: 2;
   gap: 6px;
 `;
 
 const StyledButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 12px;
+  margin-top: 6px;
+`;
+
+const StyledButtonInWrapper = styled.div`
+  position: absolute;
+  bottom: 8px;
+  right: 10px;
   display: flex;
   justify-content: flex-start;
   gap: 12px;
@@ -111,4 +201,19 @@ const StyledComment = styled.p`
     font-size: 1.2rem;
   }
   color: ${COLORS.GRAY_9F};
+`;
+
+const StyledInputWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledInput = styled.textarea`
+  width: 100%;
+  height: auto;
+  resize: none;
+  padding: 8px 8px;
+  border-radius: 8px;
+  border: 1px solid ${COLORS.VIOLET_55};
+  background-color: ${COLORS.WHITE_FF};
+  color: ${COLORS.BLACK_33};
 `;
