4b7695b9e980e391955bc7c90671d97ff68b1517
qooktree1 <qooktree@gmail.com>
2023-12-30 17:10:51 +0900

    [#67] Feat: Navbar 드랍다운 틀 작성

---
+++ components/DashboardNavbar/DarkmodeButton.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import btn from '@/public/assets/icons/AddBoxWhite.svg';
+import dark from '@/public/assets/icons/BlueEllipse.svg';
+import light from '@/public/assets/icons/PurpleEllipse.svg';
+import Image from 'next/image';
+
+function DarkmodeButton() {
+  const [isDarkmode, setIsDarkmode] = useState(false);
+
+  const handleTheme = () => {};
+
+  return (
+    <StyledContainer $isDarkmode={isDarkmode} onClick={handleTheme}>
+      {isDarkmode ? '' : <Image className="dark-icon" src={dark} alt="밝은 모드" />}
+      <Image src={btn} alt="버튼" />
+      {/* <Button className="button" /> */}
+      {isDarkmode ? <Image className="sun-icon" src={light} alt="다크 모드" /> : ''}
+    </StyledContainer>
+  );
+}
+
+export default DarkmodeButton;
+
+const StyledContainer = styled.div<{ $isDarkmode: boolean }>`
+  width: 8rem;
+  height: 4.1rem;
+  border-radius: 4rem;
+  background: ${({ $isDarkmode }) => ($isDarkmode ? '#ffc261;' : '#fff;')};
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  position: relative;
+  svg > circle {
+    fill: ${({ $isDarkmode }) => ($isDarkmode ? '#fff;' : '#000;')};
+  }
+  .button {
+    position: absolute;
+    top: 3px;
+    left: ${({ $isDarkmode }) => ($isDarkmode ? '3px;' : '42px;')};
+    transition: all 0.3s ease-in-out;
+  }
+  .dark-icon {
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    visibility: ${({ $isDarkmode }) => ($isDarkmode ? 'hidden;' : 'visible;')};
+    transition: all 0.3s ease-in-out;
+  }
+  .sun-icon {
+    position: absolute;
+    top: 5px;
+    left: 44px;
+    visibility: ${({ $isDarkmode }) => ($isDarkmode ? 'visible;' : 'hidden;')};
+    transition: all 0.3s ease-in-out;
+  }
+`;
--- components/DashboardNavbar/DashboardDropdown.tsx
+++ components/DashboardNavbar/DashboardDropdown.tsx
@@ -1,58 +1,115 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-import useUserData from '@/hooks/global/useUserData';
 import { fontStyle } from '@/styles/fontStyle';
 import { COLORS } from '@/styles/palettes';
-import { localStorageRemoveItem } from '@/utils/localStorage';
+import { localStorageClear } from '@/utils/localStorage';
 
-function DashboardDropdown() {
-  const { setUser } = useUserData();
+import UserImage from '@/public/assets/icons/user.svg';
+import LogoutImage from '@/public/assets/icons/Logout.svg';
+import DashboardImage from '@/public/assets/icons/Dashboard.svg';
+import Image from 'next/image';
+import DarkmodeButton from './DarkmodeButton';
 
+interface DashboardDropdownProps {
+  deviceType: string | undefined;
+  isOpen: boolean;
+}
+
+function DashboardDropdown({ deviceType, isOpen }: DashboardDropdownProps) {
   const logout = () => {
-    setUser({
-      createdAt: '',
-      email: '',
-      id: 0,
-      nickname: '',
-      profileImageUrl: '',
-      updatedAt: '',
-    });
-    localStorageRemoveItem('accessToken');
+    if (window.confirm('로그아웃 하시겠습니까?')) {
+      localStorageClear();
+    }
   };
 
+  const enableDarkmode = () => {};
+
   return (
-    <StyledContainer>
-      <StyledLink href="/" onClick={() => logout()}>
-        로그아웃
-      </StyledLink>
-      <StyledLink href="/mypage">내 정보</StyledLink>
-      <StyledLink href="/myboard">내 대시보드</StyledLink>
+    <StyledContainer $deviceType={deviceType} $isOpen={isOpen}>
+      <ul>
+        <li>
+          <Image width={25} height={25} src={UserImage} alt="내 정보" />
+          <StyledLink href="/mypage">내 정보</StyledLink>
+        </li>
+        <li>
+          <Image width={25} height={25} src={DashboardImage} alt="내 대시보드" />
+          <StyledLink href="/myboard">내 대시보드</StyledLink>
+        </li>
+        <li>
+          <Image width={25} height={25} src={LogoutImage} alt="로그아웃" />
+          <StyledLink href="/" onClick={() => logout()}>
+            로그아웃
+          </StyledLink>
+        </li>
+        <StyledSeperator></StyledSeperator>
+        <li>
+          <DarkmodeButton />
+        </li>
+        {/* <li><StyledWrapper onClick={enableDarkmode}>다크모드</StyledWrapper></li> */}
+      </ul>
     </StyledContainer>
   );
 }
 
 export default DashboardDropdown;
 
-const StyledContainer = styled.div`
+const StyledContainer = styled.div<{ $deviceType: string | undefined; $isOpen: boolean }>`
+  display: ${({ $isOpen }) => ($isOpen ? 'flex' : 'none')};
   position: absolute;
-  width: 100px;
-  height: min-content;
+  width: 217px;
+  height: auto;
   background-color: ${COLORS.WHITE_FF};
-  top: 40px;
-  right: 0;
-  display: flex;
+  border: 1px solid ${COLORS.GRAY_D9};
+  top: 50px;
+  /* right: 0px; */
+  right: ${({ $deviceType }) => ($deviceType === 'mobile' ? 10 : 10)}px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid ${COLORS.GRAY_78};
-  border-radius: 2px;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
+  border-radius: 6px;
+  padding: 13px 8px;
+  z-index: 3;
+
+  ul {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  ul > li {
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+    margin-top: 10px;
+    gap: 10px;
+    width: 100%;
+    ${fontStyle(16, 100)}
+    &:hover {
+      background-color: ${COLORS.GRAY_F5};
+    }
+  }
 `;
 
 const StyledLink = styled(Link)`
   width: 100%;
-  ${fontStyle(16, 200)}
+  ${fontStyle(14, 100)}
   &:hover {
     background-color: ${COLORS.GRAY_F5};
   }
+`;
+
+const StyledWrapper = styled.div`
+  cursor: pointer;
+  width: 100%;
+  ${fontStyle(14, 100)}
+  &:hover {
+    background-color: ${COLORS.GRAY_F5};
+  }
+`;
+
+const StyledSeperator = styled.div`
+  border: 1px solid black;
 `;
