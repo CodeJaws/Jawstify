246183ca4ded6d8697a011c64821401f63c8ce74
Soeun Kim <summerkim0825@gmail.com>
2023-12-31 14:19:32 +0900

    [#107] Feat: 다크모드 색상 임시 지정

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -3,6 +3,23 @@ import { reset } from './reset';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
+
+  body {
+    background-color: var(--bg-main);
+    transition: background 0.2s ease-in, color 0.2s ease-in;
+  }
+
+  body[data-theme="light"] {
+
+    // background(landingPage)
+    --bg-main: #FFFFFF;
+  }
+
+  body[data-theme="dark"] {
+
+    // main(landingPage)
+    --bg-main: #000000;
+  }
 `;
 
 export default GlobalStyles;
