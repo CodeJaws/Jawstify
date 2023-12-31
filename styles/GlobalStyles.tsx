88e8fad0425063852821e146ecf177a3b42946e3
Soeun Kim <summerkim0825@gmail.com>
2024-01-01 02:25:30 +0900

    [#107] Style: theme.ts 파일 분리

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,40 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 import { reset } from './reset';
+import { theme } from './theme';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
-
-  body {
-    background-color: var(--bg-main);
-    color: var(--text-main);
-    transition: background 0.2s ease-in, color 0.2s ease-in;
-  }
-
-  body[data-theme="light"] {
-    // landingPage
-    --bg-main: #FFFFFF;
-    --bg-second: #F1EFFD;
-    --bg-third: #F1EFFD;
-    --bg-fourth: #1501cb9c;
-    --bg-footer: #171717;
-
-    --text-main: #000000;
-    --text-second: #333236;
-    --text-third: #171717;
-  }
-
-  body[data-theme="dark"] {
-    // landingPage
-    --bg-main: #000000;
-    --bg-second: #171717;
-    --bg-third: #4B4B4B;
-    --bg-fourth: #171717;
-    --bg-footer: #000000;
-
-    --text-main: #FFFFFF;
-    --text-second: #9FA6B2;
-    --text-third: #FFFFFF;
-  }
+  ${theme}
 `;
 
 export default GlobalStyles;
