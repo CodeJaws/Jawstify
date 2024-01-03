26f8cb3d32aa55dad31c78dfb72cc93f3ed767a6
arky02 <kyean07@naver.com>
2024-01-04 03:35:13 +0900

    [#80] Fix: merge 충돌 해결

--- hooks/DropDown/useManager.ts
+++ hooks/DropDown/useManager.ts
@@ -1,12 +1,12 @@
 import { create } from 'zustand';
 
 interface Props {
-  manager: number | null;
+  manager: number;
   setManager: (e: number) => void;
 }
 
 const useManager = create<Props>((set) => ({
-  manager: null,
+  manager: 0, // TODO: 이 부분 현재 로그인 된 유저 아이디로 가져오게 수정해야함
   setManager: (value: number) => set({ manager: value }),
 }));
 
