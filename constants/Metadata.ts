724bd78ce136a910572d0ecf24db806d0a5ada5b
oauch <lgm9515@naver.com>
2024-01-04 13:10:46 +0900

    [#151] Feat: MetaData 적용 및 아이콘 적용

---
+++ constants/Metadata.ts
@@ -0,0 +1,9 @@
+import mainLogo from '@/public/assets/images/jaws.png';
+
+export const METADATA = {
+  title: 'Jawstify',
+  description: '일정관리 조습니다🦈',
+  url: 'https://taskify4.vercel.app/',
+  image: mainLogo.src,
+  locale: 'ko_KR',
+};
