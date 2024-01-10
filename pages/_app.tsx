64847fcc24defe96086bed4d8088e5472c9afd1a
arky02 <kyean07@naver.com>
2024-01-10 23:09:42 +0900

    [#195] Fix: tanstack 초기 설정 복구

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 /* eslint-disable @next/next/inline-script-id */
 import GlobalStyles from '@/styles/GlobalStyles';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Script from 'next/script';
 import * as gtag from '@/lib/gtag';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
@@ -22,8 +35,11 @@ export default function App({ Component, pageProps }: AppProps) {
           `,
         }}
       />
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
