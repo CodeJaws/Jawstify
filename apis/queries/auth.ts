cb8cebf91b5f0799b6fa82e1bef03fae1d4b7896
arky02 <kyean07@naver.com>
2024-01-16 02:56:50 +0900

    [#197] auth react-query 기초 api 코드 적용

--- apis/queries/auth.ts
+++ apis/queries/auth.ts
@@ -1,5 +1,30 @@
+import { handleReactQueryError } from '@/lib/toast';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ErrorProps } from '@/types/api';
+import { useRouter } from 'next/router';
+import { request } from '../axios';
+import { LoginProps } from '@/types/api';
+
 /** 로그인 */
-export const useLogin = () => {};
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const { mutate, isPending, isError, error, data } = useMutation({
+    mutationFn: ({ email, password }: LoginProps) => {
+      return request.post(`auth/login`, { email, password });
+    },
+    onSuccess: () => {
+      // user별로 세분화시키기
+      queryClient.invalidateQueries({ queryKey: ['loginData'] });
+      alert('로그인 성공!');
+      router.push('mydashboard');
+    },
+    onError: (error) => handleReactQueryError(error as unknown as ErrorProps),
+  });
+
+  return { mutate, isPending, isError, error, data };
+};
 
 /** 비밀번호 변경 */
 export const useChangePassword = () => {};
