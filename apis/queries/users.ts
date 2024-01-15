df33ebf09a2d395d72303c763c355dd04263c393
arky02 <kyean07@naver.com>
2024-01-16 02:57:24 +0900

    [#197] users react-query 기초 api 코드 적용

--- apis/queries/users.ts
+++ apis/queries/users.ts
@@ -1,5 +1,24 @@
+import useColumnId from '@/hooks/ModalCard/useColumnId';
+import { ErrorProps, SignupProps } from '@/types/api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { request } from '../axios';
+import { handleReactQueryError } from '@/lib/toast';
+
 /** 회원가입 */
-export const useSignup = () => {};
+export const useSignup = (email: string) => {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: (body: SignupProps) => request.post('users', body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['signUpData', email] });
+      console.log('회원가입 완료!');
+    },
+    onError: (error) => handleReactQueryError(error as unknown as ErrorProps),
+  });
+
+  return { mutate, isPending, isError, error };
+};
 
 /** 내 정보 조회 */
 export const useGetMyInfo = () => {};
