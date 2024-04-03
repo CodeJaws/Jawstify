6b1c09fa54e8a86b947c5ef1861f9e7b09ef094c
oauch <lgm9515@naver.com>
2024-04-03 18:41:34 +0900

    [#220] refactor: 메타 태그 변경 및 타이틀 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,3 @@
-import { METADATA } from '@/constants/Metadata';
-
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -38,20 +36,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head>
-          <title>Jawstify</title>
-          <link rel="icon" href={METADATA.IMAGE} sizes="any" />
-          <meta property="og:title" content={METADATA.TITLE} />
-          <meta property="og:url" content={METADATA.URL} />
-          <meta property="og:image" content={METADATA.IMAGE} />
-          <meta property="og:description" content={METADATA.DESCRIPTION} />
-          <meta name="twitter:card" content="summary" />
-          <meta property="twitter:domain" content={METADATA.DOMAIN} />
-          <meta property="twitter:url" content={METADATA.URL} />
-          <meta name="twitter:title" content={METADATA.TITLE} />
-          <meta name="twitter:description" content={METADATA.DESCRIPTION} />
-          <meta name="twitter:image" content={METADATA.IMAGE} />
-        </Head>
+        <Head></Head>
         <body>
           <script dangerouslySetInnerHTML={{ __html: themeInitializerScript }} />
           <Main />
